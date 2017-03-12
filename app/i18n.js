a5550704f56bbd4767fe58bfc8fd0940a5bda44d
Damien Goujard <damien@macuser.fr>
2017-03-12 19:29:00 +0100

    Add French translation

--- app/i18n.js
+++ app/i18n.js
@@ -7,18 +7,22 @@
 import { addLocaleData } from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
 import zhLocaleData from 'react-intl/locale-data/zh';
+import frLocaleData from 'react-intl/locale-data/fr';
 
 import { DEFAULT_LOCALE } from './containers/App/constants'; // eslint-disable-line
 import enTranslationMessages from './translations/en.json';
 import zhTranslationMessages from './translations/zh.json';
+import frTranslationMessages from './translations/fr.json';
 
 export const appLocales = [
   'en',
   'zh',
+  'fr',
 ];
 
 addLocaleData(enLocaleData);
 addLocaleData(zhLocaleData);
+addLocaleData(frLocaleData);
 
 export const formatTranslationMessages = (locale, messages) => {
   const defaultFormattedMessages = locale !== DEFAULT_LOCALE
@@ -36,4 +40,5 @@ export const formatTranslationMessages = (locale, messages) => {
 export const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
   zh: formatTranslationMessages('zh', zhTranslationMessages),
+  fr: formatTranslationMessages('fr', frTranslationMessages),
 };
